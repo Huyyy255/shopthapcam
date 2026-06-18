42e7d002e7b9c3efbabdf959f4feecf5a3a74483
Huyy255 <huy297746@gmail.com>
2026-06-18 23:26:35 +0700

    Handle ajaxs/ PHP files via api/index.php proxy

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (preg_match('#\.php$#', $requestPath) && $requestPath !== '/api/index.php') {
+    $file = realpath(__DIR__ . '/..' . $requestPath);
+    $allowedDirs = [realpath(__DIR__ . '/../ajaxs'), realpath(__DIR__ . '/../cron'), realpath(__DIR__ . '/../')];
+    foreach ($allowedDirs as $dir) {
+        if ($file && strpos($file, $dir) === 0 && file_exists($file)) {
+            require $file;
+            exit;
+        }
+    }
+}
 require_once __DIR__ . '/../index.php';
